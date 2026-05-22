4439839d7de1bd316400d2297af61605a375bfe3
kumarvinesh28 <soothar.vinesh@gmail.com>
2026-05-22 12:39:47 +0500

    initial commit

--- src/interviewPrep/Main.java
+++ src/interviewPrep/Main.java
@@ -2,14 +2,16 @@ package interviewPrep;
 
 public class Main {
 
-    public void add(int a, int b){
-        System.out.println(a+b);
-    }
-    public void add(Integer a, Integer b){
-        System.out.println(a+b);
-    }
+//    public void add(int a, int b){
+//        System.out.println(a+b);
+//    }
+//    public void add(Integer a, Integer b){
+//        System.out.println(a+b);
+//    }
     public static void main(String[] args) {
-     Main main = new Main();
-
+//     Main main = new Main();
+int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
     }
 }
